f64c16a7224b6c31963f777e3857a9bb4a626d18
Maarten Paauw <maartenpaauw@gmail.com>
2022-01-05 16:11:14 +0100

    chore: add length specification for testing purpose

---
+++ tests/Dummy/LengthSpecification.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Maartenpaauw\Specifications\Tests\Dummy;
+
+use Maartenpaauw\Specifications\CompositeSpecification;
+
+/**
+ * @extends CompositeSpecification<string>
+ */
+class LengthSpecification extends CompositeSpecification
+{
+    public function __construct(private int $length)
+    {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isSatisfiedBy(mixed $candidate): bool
+    {
+        return strlen($candidate) === $this->length;
+    }
+}
---
+++ tests/Dummy/LengthSpecificationTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Maartenpaauw\Specifications\Tests\Dummy;
+
+use Maartenpaauw\Specifications\Tests\TestCase;
+
+class LengthSpecificationTest extends TestCase
+{
+    private LengthSpecification $specification;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->specification = new LengthSpecification(12);
+    }
+
+    /** @test */
+    public function it_should_return_true_when_the_given_string_length_is_equals(): void
+    {
+        // Act
+        $satisfied = $this->specification->isSatisfiedBy('Hello world!');
+
+        // Assert
+        $this->assertTrue($satisfied);
+    }
+
+    /** @test */
+    public function it_should_return_false_when_the_given_string_does_not_match_the_given_length(): void
+    {
+        // Act
+        $satisfied = $this->specification->isSatisfiedBy('Hello Laravel!');
+
+        // Assert
+        $this->assertFalse($satisfied);
+    }
+}
