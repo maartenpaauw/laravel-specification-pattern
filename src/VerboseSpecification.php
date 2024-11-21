e6af9caf3e2d0634b47c25a16f4513c12f0c08e8
Maarten Paauw <maartenpaauw@gmail.com>
2024-11-21 08:55:37 +0100

    chore: use `@inheritDoc` instead of copying the parent docblock

--- src/VerboseSpecification.php
+++ src/VerboseSpecification.php
@@ -57,7 +57,7 @@ final class VerboseSpecification extends CompositeSpecification
     }
 
     /**
-     * @param  TCandidate  $candidate
+     * @inheritDoc
      *
      * @throws DissatisfiedSpecification
      */
